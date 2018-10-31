ce9a8ad29c51da4ca5e9a9b74cb8f64d0b63d2c9
Miragefl <silence_fl@yeah.net>
2018-10-31 21:01:53 +0800

    11

--- site/src/main/java/com/soap/site/service/ColumnService.java
+++ site/src/main/java/com/soap/site/service/ColumnService.java
@@ -1,15 +1,13 @@
 package com.soap.site.service;
 
-import com.soap.constant.Const;
-import com.soap.exception.BizFailException;
 import com.soap.site.mapper.ColumnMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -23,10 +21,12 @@ public class ColumnService {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    private static final String COLUMN_CACHE_NAME = "column";
+
     @Autowired
     private ColumnMapper columnMapper;
 
-
+    @Cacheable(value =COLUMN_CACHE_NAME, key = "column")
     public List<Map<String, Object>> qryColumns(Map<String, Object> params) {
         List<Map<String, Object>> list = qry(null);
         for (int i=0;i<list.size();i++) {
